00cc1787040f4464c59260a878818a3ab499a6f2
ThomassMathisen <thomasmathisen@hotmail.com>
2022-11-28 12:06:01 +0100

    Create featuredInfo component for homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
+import Home from "./pages/home/Home";
+import "./app.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Topbar />
-      <Sidebar />
-    </div>
+      <div className="container">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
