2d85ae521ac28cd7a725684b43f2f235416c6af8
ThomassMathisen <thomasmathisen@hotmail.com>
2022-11-28 18:56:37 +0100

    Create user page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/home/Home";
 import "./app.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/users" element={<UserList />} />
+          <Route path="/user/:userId" element={<User />} />
         </Routes>
       </div>
     </Router>
